c4a2f14fcdc0b76c44b955668770c930651e75b4
Alexander Mills <alex@oresoftware.com>
2026-09-09 15:33:46 -0500

    DEN-1863: apply rustfmt to interface exports

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,9 @@ pub mod schema;
 
 pub use build_log::{
     LogSidecarCommandStarted, LogSidecarCommandTerminal, LogSidecarFrameDescriptor,
-    LogSidecarFrameStream, LogSidecarStartEvent, LogSidecarTerminalEvent,
-    LogSidecarWireVector, LOG_SIDECAR_FRAME_HEADER_BYTES, LOG_SIDECAR_FRAME_MAGIC,
-    LOG_SIDECAR_FRAME_VERSION, LOG_SIDECAR_MAX_PAYLOAD_BYTES,
-    LOG_SIDECAR_MAX_SHUTDOWN_MILLIS, LOG_SIDECAR_PROTOCOL,
+    LogSidecarFrameStream, LogSidecarStartEvent, LogSidecarTerminalEvent, LogSidecarWireVector,
+    LOG_SIDECAR_FRAME_HEADER_BYTES, LOG_SIDECAR_FRAME_MAGIC, LOG_SIDECAR_FRAME_VERSION,
+    LOG_SIDECAR_MAX_PAYLOAD_BYTES, LOG_SIDECAR_MAX_SHUTDOWN_MILLIS, LOG_SIDECAR_PROTOCOL,
     LOG_SIDECAR_STDERR_STREAM_ID, LOG_SIDECAR_STDOUT_STREAM_ID,
 };
 pub use error::InterfaceError;
